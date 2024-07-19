fdde7f3f3ee8ff59b562b0d0d3e2cb33cbe5112b
Dongjin Kim <djkim9031.research@gmail.com>
2024-07-19 00:06:57 +0000

    function to read all images and pixels implemented

--- src/dataprep.h
+++ src/dataprep.h
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
 
 struct BGR{
     // bmp color channel order
@@ -52,5 +53,50 @@ inline int BMPReader(const char* filename, const int width, const int height, BG
     return 0;
 }
 
+
+inline int ReadAllBMPsInDirectory(const char* dirPath, BGR*** allPixels, int& nImages, int width, int height){
+    DIR* dir = opendir(dirPath);
+    if(!dir){
+        perror("Unable to open the given directory.");
+        return -1;
+    }
+
+    struct dirent* entry;
+    nImages = 0;
+    while((entry = readdir(dir)) != NULL){
+        if(entry->d_type == DT_REG && strstr(entry->d_name, ".bmp")){
+            nImages++;
+        }
+    }
+    rewinddir(dir); // Rewind the directory pointer to read all bmp images.
+
+    *allPixels = (BGR**)malloc(nImages*sizeof(BGR*));
+    if(*allPixels == NULL){
+        perror("Unable to allocate memory");
+        closedir(dir);
+        return -1;
+    }
+
+    int idx = 0;
+    while((entry = readdir(dir)) != NULL){
+        if (entry->d_type == DT_REG && strstr(entry->d_name, ".bmp")){
+            char filePath[1024];
+            snprintf(filePath, sizeof(filePath), "%s/%s", dirPath, entry->d_name);
+
+            BGR* pixels = NULL;
+            // Read the BMP file to get pixel data
+            if (BMPReader(filePath, width, height, &pixels) == 0){
+                (*allPixels)[idx] = pixels;
+                idx++;
+            } else{
+                printf("Image %d failed to read pixels", idx);
+                free(pixels);
+            }
+        }
+    }
+
+    closedir(dir);
+    return 0;
+}
 
 
