5335405ad3acdb6a8d39680cb0bf5b42b0437cb6
Dongjin Kim <djkim9031.research@gmail.com>
2024-07-26 18:17:12 +0000

    `load_experiment` function added

--- python/utils.py
+++ python/utils.py
@@ -36,3 +36,26 @@ def save_checkpoint(experiment_name, model, epoch, base_dir="experiments"):
     os.makedirs(outdir, exist_ok=True)
     cpfile = os.path.join(outdir, f'model_{epoch}.pt')
     torch.save(model.state_dict(), cpfile)
+
+def load_experiment(experiment_name, checkpoint_name="model_final.pt", base_dir="experiments"):
+    outdir = os.path.join(base_dir, experiment_name)
+
+    # Load the config
+    configfile = os.path.join(outdir, 'config.json')
+    with open(configfile, 'r') as f:
+        config = json.load(f)
+
+    # Load the metrics
+    jsonfile = os.path.join(outdir, 'metrics.json')
+    with open(jsonfile, 'r') as f:
+        data = json.load(f)
+    
+    train_losses = data['train_losses']
+    test_losses = data['test_losses']
+    accuracies = data['accuracies']
+
+    # Load the model
+    model = ViT(config)
+    cpfile = os.path.join(outdir, checkpoint_name)
+    model.load_state_dict(torch.load(cpfile))
+    return config, model, train_losses, test_losses, accuracies
