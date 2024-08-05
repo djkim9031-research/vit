eb0a7ec9258b42b9cdd3f6186d1bfc74e4dcb278
Dongjin Kim <djkim9031.research@gmail.com>
2024-08-05 17:17:08 -0400

    layernorm CUDA implementation

--- cuda/layernorm.cu
+++ cuda/layernorm.cu
@@ -41,6 +41,53 @@ __global__ void layernorm_forward_kernal1(float* x, float* mean, float* rstd,
     }
 }
 
+__global__ void layernorm_backward_kernal1(float* x, float* mean, float* rstd, float* weight,
+                                           float* dx, float* dweight, float* dbias, float* dy,
+                                           int N, int H){
+    
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if(idx < N){
+        float curr_mean = mean[idx];
+        float curr_rstd = rstd[idx];
+
+        // Obtain derivatives of norm (n = s*(x - m) in forward pass)
+        // to reduce ops
+        float dnorm_mean = 0.f; // mean of dn
+        float dnorm_norm_mean = 0.f; // mean of dn * n
+        for(int h=0; h<H; ++h){
+            float n = curr_rstd * (x[idx*H + h] - curr_mean);
+            // since y = weight * n + bias, n' = (dn) = dy* weight
+            float dn = dy[idx*H + h] * weight[h];
+            dnorm_mean += dn;
+            dnorm_norm_mean += dn * n;
+        }
+        dnorm_mean /= H;
+        dnorm_norm_mean /= H;
+
+        // Calculate the gradients
+        for(int h=0; h<H; ++h){
+            float n = curr_rstd * (x[idx*H + h] - curr_mean); 
+            float dn = dy[idx*H + h] * weight[h];
+
+            // bias derivative
+            atomicAdd(&dbias[h], dy[idx*H + h]);
+            // weight derivative
+            atomicAdd(&dweight[h], dy[idx*H + h]*n);
+            // x derivative
+            // dx = dn * rstd + sum_over_H(dn * -rstd * 1/H) - (x - mean)*sum_over_H(dn * (x-mean)*rstd^3 / H)
+            //    = rstd*(dn -1/H *sum_over_H(dn) - (x-mean)*sum_over_H(dn*(x-mean)*rstd^2 / H))
+            //    = rstd*(dn -1/H *sum_over_H(dn) - (x-mean)*rstd*sum_over_H(dn*n/H))
+            //    = rstd(dn - dnorm_mean - n * dnorm_norm_mean)
+            float dval = 0.f;
+            dval += dn; // term 1
+            dval -= dnorm_mean; // term 2
+            dval -= n * dnorm_norm_mean; // term 3
+            dval *= curr_rstd; // scale
+            dx[idx*H + h] += dval;
+        }
+    }
+}
+
 // -----------------------------------------------------------------------------------------
 // kernel launcher
 
@@ -50,4 +97,12 @@ void layernorm_forward1(float* x, float* mean, float* rstd,
     const int N = B*T;
     const int grid_size = ceil_div(N, block_size);
     layernorm_forward_kernal1<<<grid_size, block_size>>>(x, mean, rstd, weight, bias, y, N, H);
+}
+
+void layernorm_backward1(float* x, float* mean, float* rstd, float* weight,
+                         float* dx, float* dweight, float* dbias, float* dy,
+                         int B, int T, int H, const int block_size){
+    const int N = B*T;
+    const int grid_size = ceil_div(N, block_size);
+    layernorm_backward_kernal1<<<grid_size, block_size>>>(x, mean, rstd, weight, dx, dweight, dbias, dy, N, H);
 }
--- cuda/layernorm.cuh
+++ cuda/layernorm.cuh
@@ -5,7 +5,7 @@
 // -----------------------------------------------------------------------------------------
 // GPU kernels
 
-// Layernorm function, forward kernal function 1.
+// Layernorm function, forward kernal function 1
 // Layer normalization over the given input tensor
 // H dimension vector of activations gets normalized, then scaled and shifted.
 //
@@ -21,6 +21,23 @@
 __global__ void layernorm_forward_kernal1(float* x, float* mean, float* rstd,
                                           float* weight, float* bias, float* y,
                                           int N, int H);
+
+// Layernorm function, backward kernal function 1
+//
+// @param x             linearized input tensors (batch_size B, sequence length T, hidden_size = H)
+// @param mean          linearized mean tensors over the last dimension (hidden size dim) [B, T]
+// @param rstd          linearized reciprocal standard deviation tensors (B, T)
+// @param weight        linearized weight(scale) tensor parameters (H)
+// @param dx            linearized input tensor derivatives
+// @param dweight       linearized weight tensor derivatives
+// @param dbias         linearized bias tensor derivatives
+// @param dy            linearized output tensor derivatives
+// @param N             number of batches(B) x sequence length (T)
+// @param H             hidden size
+//
+__global__ void layernorm_backward_kernal1(float* x, float* mean, float* rstd, float* weight,
+                                           float* dx, float* dweight, float* dbias, float* dy,
+                                           int N, int H);
 
 // -----------------------------------------------------------------------------------------
 // kernel launcher
@@ -41,3 +58,22 @@ __global__ void layernorm_forward_kernal1(float* x, float* mean, float* rstd,
 void layernorm_forward1(float* x, float* mean, float* rstd,
                         float* weight, float* bias, float* y,
                         int B, int T, int H, const int block_size);
+
+// Layernorm backward kernal launcher 1
+//
+// @param x             linearized input tensors (batch_size B, sequence length T, hidden_size = H)
+// @param mean          linearized mean tensors over the last dimension (hidden size dim) [B, T]
+// @param rstd          linearized reciprocal standard deviation tensors (B, T)
+// @param weight        linearized weight(scale) tensor parameters (H)
+// @param dx            linearized input tensor derivatives
+// @param dweight       linearized weight tensor derivatives
+// @param dbias         linearized bias tensor derivatives
+// @param dy            linearized output tensor derivatives
+// @param B             number of batches
+// @param T             sequence length
+// @param H             hidden size
+// @param block_size    CUDA block size
+//
+void layernorm_backward1(float* x, float* mean, float* rstd, float* weight,
+                         float* dx, float* dweight, float* dbias, float* dy,
+                         int B, int T, int H, const int block_size);
