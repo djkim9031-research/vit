d750e63e55c0c417c463064a38b1898194300ad0
Dongjin Kim <djkim9031.research@gmail.com>
2024-08-05 14:47:59 -0400

    naive CUDA matmul functions implemented

--- cuda/matmul.cu
+++ cuda/matmul.cu
@@ -1,5 +1,9 @@
 #include "matmul.cuh"
 
+
+// -----------------------------------------------------------------------------------------
+// GPU kernels
+
 __global__ void matmul_forward_kernel1(float* x, float* y, float* weight, float* bias,
                                        int B_in_r, int in_c, int ou_c){
     
@@ -15,10 +19,41 @@ __global__ void matmul_forward_kernel1(float* x, float* y, float* weight, float*
     }
 }
 
+__global__ void matmul_backward_kernel1(float* x, float* weight, float* dx, float* dweight, float* dbias,
+                                        float* dy, int B_in_r, int in_c, int ou_c){
+    
+    int B_in_r_dim = blockIdx.x * blockDim.x + threadIdx.x;
+    int ou_c_dim = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if(B_in_r_dim < B_in_r && ou_c_dim < ou_c){
+        float grad = dy[B_in_r_dim*ou_c + ou_c_dim];
+
+        if(dbias != NULL){
+            atomicAdd(&dbias[ou_c_dim], grad);
+        }
+
+        for(int ic = 0; ic < in_c; ++ic){
+            atomicAdd(&dx[B_in_r_dim*in_c + ic], grad * weight[ic*ou_c + ou_c_dim]);
+            atomicAdd(&dweight[ic*ou_c + ou_c_dim], grad * x[B_in_r_dim*in_c + ic]);
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------------------
+// kernel launcher
+
 void matmul_forward1(float* x, float* y, float* weight, float* bias,
                      int B, int in_r, int in_c, int ou_c, const int sqrt_block_size){
     
     dim3 gridDim(ceil_div(B*in_r, sqrt_block_size), ceil_div(ou_c, sqrt_block_size));
     dim3 blockDim(sqrt_block_size, sqrt_block_size);
     matmul_forward_kernel1<<<gridDim, blockDim>>>(x, y, weight, bias, B*in_r, in_c, ou_c);
+}
+
+void matmul_backward1(float* x, float* weight, float* dx, float* dweight, float* dbias,
+                      float* dy, int B, int in_r, int in_c, int ou_c, const int sqrt_block_size){
+    
+    dim3 gridDim(ceil_div(B*in_r, sqrt_block_size), ceil_div(ou_c, sqrt_block_size));
+    dim3 blockDim(sqrt_block_size, sqrt_block_size);
+    matmul_backward_kernel<<<gridDim, blockDim>>>(x, weight, dx, dweight, dbias, dy, B*in_r, in_c, ou_c);
 }
--- cuda/matmul.cuh
+++ cuda/matmul.cuh
@@ -22,6 +22,19 @@ __global__ void matmul_forward_kernel1(float* x, float* y, float* weight, float*
                                        int B_in_r, int in_c, int ou_c);
 
 
+// Matrix multiplication, backward kernel function 1
+// @param x             linearized input tensors [B*in_r, in_c]
+// @param weight        linearized weight tensors [in_c, ou_c]
+// @param dx            linearized input tensor derivatives [B*in_r, in_c]
+// @param dweight       linearized weight tensor derivatives [in_c, ou_c]
+// @param dbias         linearized bias tensor derivatives [ou_c]
+// @param dy            linearized output tensor derivatives [B*in_r, ou_c]
+// @param B_in_r        number of batches * input row dimensions
+// @param in_c          input col dimensions
+// @param ou_c          output col dimensions
+//
+__global__ void matmul_backward_kernel1(float* x, float* weight, float* dx, float* dweight, float* dbias,
+                                        float* dy, int B_in_r, int in_c, int ou_c);
 
 // -----------------------------------------------------------------------------------------
 // kernel launcher
@@ -42,3 +55,18 @@ void matmul_forward1(float* x, float* y, float* weight, float* bias,
                      int B, int in_r, int in_c, int ou_c, const int sqrt_block_size);
 
 
+// Matmul backward kernel launcher 1
+// @param x             linearized input tensors [B*in_r, in_c]
+// @param weight        linearized weight tensors [in_c, ou_c]
+// @param dx            linearized input tensor derivatives [B*in_r, in_c]
+// @param dweight       linearized weight tensor derivatives [in_c, ou_c]
+// @param dbias         linearized bias tensor derivatives [ou_c]
+// @param dy            linearized output tensor derivatives [B*in_r, ou_c]
+// @param B             number of batches
+// @param in_r          input row dimensions
+// @param in_c          input col dimensions
+// @param ou_c          output col dimensions
+// @param sqrt_block_size  sqrt of CUDA block size
+//
+void matmul_backward1(float* x, float* weight, float* dx, float* dweight, float* dbias,
+                      float* dy, int B, int in_r, int in_c, int ou_c, const int sqrt_block_size)
