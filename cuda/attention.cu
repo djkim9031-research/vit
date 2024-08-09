0cbbdda89e2aebdda71418cee9f07d62cad2714c
Dongjin Kim <djkim9031.research@gmail.com>
2024-08-09 14:28:59 -0400

    attention naive CUDA functions implemented and tested

--- cuda/attention.cu
+++ cuda/attention.cu
@@ -95,6 +95,60 @@ __global__ void attention_value_kernel1(float* x, float* attn, float* y,
 }
 
 
+__global__ void attention_backward_kernel1(float* x, float* attn, 
+                                           float* dx, float* dpreattn, float* dattn, float* dy,
+                                           int B, int T, int H, int NH){
+    
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int total_threads = B*T*NH;
+
+    int nh = idx % NH;
+    int t1 = (idx/NH)%T;
+    int b = idx/(NH*T);
+    int HS = H / NH; // head size
+    float scale = 1.0/sqrtf(HS);
+
+    if(idx < total_threads){
+        float* dquery = dx + b*T*3*H + t1*3*H + nh*HS;
+        float* query = x + b*T*3*H + t1*3*H + nh*HS;
+
+        // backward to get dvalue and dattn
+        for(int t2=0; t2<T; ++t2){
+            float* dvalue = dx + b*T*3*H + t2*3*H + nh*HS + 2*H;
+            float* value = x + b*T*3*H + t2*3*H + nh*HS + 2*H;
+            for(int i=0; i<HS; ++i){
+                // y[i] = attn[i] * value[i]
+                atomicAdd(&dattn[b*NH*T*T + nh*T*T + t1*T + t2], value[i] * dy[b*T*H + t1*H + nh*HS + i]);
+                atomicAdd(&dvalue[i], attn[b*NH*T*T + nh*T*T + t1*T + t2] * dy[b*T*H + t1*H + nh*HS + i]);
+            }
+        }
+
+        // backward pass through softmax
+        // given input [x1, x2, ... xk], the partial derivative is
+        // given as (d(softmax(xi))/d(xk) = ):
+        // 1. if i == k, softmax(xi)*(1-softmax(xi))
+        // 2. otherwise, -softmax(xi)*softmax(xk)
+        for(int t2=0; t2<T; ++t2){
+            for(int t3=0; t3<T; ++t3){
+                float indicator = t2 == t3 ? 1.f : 0.f;
+                float local_derivative = attn[b*NH*T*T + nh*T*T + t1*T + t2] * (indicator - attn[b*NH*T*T + nh*T*T + t1*T + t3]);
+                atomicAdd(&dpreattn[b*NH*T*T + nh*T*T + t1*T + t3], local_derivative * dattn[b*NH*T*T + nh*T*T + t1*T + t2]);
+            }
+        }
+
+        // backward through q@k
+        for(int t2=0; t2<T; ++t2){
+            float* dkey = dx + b*T*3*H + t2*3*H + nh*HS + H;
+            float* key = x + b*T*3*H + t2*3*H + nh*HS + H;
+            for(int i=0; i<HS; ++i){
+                // preattn[i] = query[i]*key[i]*scale
+                atomicAdd(&dquery[i], dpreattn[b*NH*T*T + nh*T*T + t1*T + t2] * scale * key[i]);
+                atomicAdd(&dkey[i], dpreattn[b*NH*T*T + nh*T*T + t1*T + t2] * scale * query[i]);
+            }
+        }
+    }
+}
+
 
 // -----------------------------------------------------------------------------------------
 // kernel launcher
@@ -112,4 +166,14 @@ void attention_forward1(float* x, float* preattn, float* attn, float* y,
 
     attention_softmax_kernel1<<<num_blocks, block_size>>>(preattn, attn, B, T, NH);
     attention_value_kernel1<<<num_blocks, block_size>>>(x, attn, y, B, T, H, NH);
+}
+
+void attention_backward1(float* x, float* attn, 
+                         float* dx, float* dpreattn, float* dattn, float* dy,
+                         int B, int T, int H, int NH){
+    
+    int total_threads = B*T*NH;
+    int num_blocks = ceil_div(total_threads, block_size);
+
+    attention_backward_kernel1<<<num_blocks, block_size>>>(x, attn, dx, dpreattn, dattn, dy, B, T, H, NH);
 }
--- cuda/attention.cuh
+++ cuda/attention.cuh
@@ -47,6 +47,26 @@ __global__ void attention_value_kernel1(float* x, float* attn, float* y,
                                         int B, int T, int H, int NH);
 
 
+// Attention backward kernal function 1
+// This function is a naive CUDA implementation.
+// The qkv values are obtained in the previous stage with matmul
+// and passed to this function to calculate the attention score
+//
+// @param x                     linearized qkv input tensors (batch_size B, sequence length T, 3*hidden_size = 3H)
+// @param attn                  linearized attention scores (B, NH, T, T)
+// @param dx                    linearized qkv input tensor derivatives
+// @param dpreattn              linearized preattn derivatives
+// @param dattn                 linearized attn derivatives
+// @param dy                    linearized output derivatives
+// @param B                     number of batches
+// @param T                     sequence length
+// @param H                     hidden size
+// @param NH                    number of heads
+//
+__global__ void attention_backward_kernel1(float* x, float* attn, 
+                                           float* dx, float* dpreattn, float* dattn, float* dy,
+                                           int B, int T, int H, int NH);
+
 // -----------------------------------------------------------------------------------------
 // kernel launcher
 
@@ -66,6 +86,19 @@ void attention_forward1(float* x, float* preattn, float* attn, float* y,
                         int B, int T, int H, int NH, const int block_size);
 
 
-
-
-
+// Attention backward kernal launcher 1
+//
+// @param x                     linearized qkv input tensors (batch_size B, sequence length T, 3*hidden_size = 3H)
+// @param attn                  linearized attention scores (B, NH, T, T)
+// @param dx                    linearized qkv input tensor derivatives
+// @param dpreattn              linearized preattn derivatives
+// @param dattn                 linearized attn derivatives
+// @param dy                    linearized output derivatives
+// @param B                     number of batches
+// @param T                     sequence length
+// @param H                     hidden size
+// @param NH                    number of heads
+// @param block_size            CUDA block size
+void attention_backward1(float* x, float* attn, 
+                         float* dx, float* dpreattn, float* dattn, float* dy,
+                         int B, int T, int H, int NH);
