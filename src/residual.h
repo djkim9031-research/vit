b374f81d61ecc1cca2e08b0a355fb26a2b998427
Dongjin Kim <djkim9031.research@gmail.com>
2024-08-09 15:18:19 -0400

    comment update

--- src/residual.h
+++ src/residual.h
@@ -12,7 +12,7 @@ inline void residual_forward(float* x1, float* x2, float* y, int N){
     }
 }
 
-// Residual connection, forward function.
+// Residual connection, backward function.
 // y = x1 + x2 => dy/dx1 = 1, dy/dx2 = 1
 // @param dx1           linearized input1 derivatives
 // @param dx2           linearized input2 derivatives
