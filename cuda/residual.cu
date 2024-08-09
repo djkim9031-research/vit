af63417fd3251fb84820074b9d94711222c67927
Dongjin Kim <djkim9031.research@gmail.com>
2024-08-09 15:22:12 -0400

    residual CUDA functions implemented and tested

--- cuda/residual.cu
+++ cuda/residual.cu
@@ -13,6 +13,17 @@ __global__ void residual_forward_kernel1(float* x1, float* x2, float* y, int N){
     }
 }
 
+__global__ void residual_backward_kernel1(float* dx1, float* dx2, float* dy, int N){
+
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if(i < N){
+        // Applying chainrule: 1*dy
+        atomicAdd(&dx1[i], dy[i]);
+        atomicAdd(&dx2[i], dy[i]);
+    }
+}
+
 
 // -----------------------------------------------------------------------------------------
 // kernel launcher
@@ -21,4 +32,10 @@ void residual_forward1(float* x1, float* x2, float* y, int N, const int block_si
 
     const int grid_size = ceil_div(N, block_size);
     residual_forward_kernel1<<<grid_size, block_size>>>(x1, x2, y, N);
+}
+
+void residual_backward1(float* dx1, float* dx2, float* dy, int N, const int block_size){
+
+    const int grid_size = ceil_div(N, block_size);
+    residual_backward_kernel1<<<grid_size, block_size>>>(dx1, dx2, dy, N);
 }
--- cuda/residual.cuh
+++ cuda/residual.cuh
@@ -15,6 +15,16 @@
 __global__ void residual_forward_kernel1(float* x1, float* x2, float* y, int N);
 
 
+// Residual connection, backward kernal function 1
+//
+// y = x1 + x2 => dy/dx1 = 1, dy/dx2 = 1
+// @param dx1           linearized input1 derivatives
+// @param dx2           linearized input2 derivatives
+// @param dy            linearized output derivatives
+// @param N             number of elements
+//
+__global__ void residual_backward_kernel1(float* dx1, float* dx2, float* dy, int N);
+
 
 // -----------------------------------------------------------------------------------------
 // kernel launcher
@@ -28,3 +38,14 @@ __global__ void residual_forward_kernel1(float* x1, float* x2, float* y, int N);
 // @param block_size    CUDA block size
 //
 void residual_forward1(float* x1, float* x2, float* y, int N, const int block_size);
+
+// Residual connection backward kernal launcher 1
+//
+// y = x1 + x2 => dy/dx1 = 1, dy/dx2 = 1
+// @param dx1           linearized input1 derivatives
+// @param dx2           linearized input2 derivatives
+// @param dy            linearized output derivatives
+// @param N             number of elements
+// @param block_size    CUDA block size
+//
+void residual_backward1(float* dx1, float* dx2, float* dy, int N, const int block_size);
