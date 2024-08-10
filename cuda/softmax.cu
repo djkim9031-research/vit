fa0d9acbfbc3017ad6095402b47a8c737915ed4e
Dongjin Kim <djkim9031.research@gmail.com>
2024-08-10 14:49:41 -0400

    softmax naive CUDA functions implemented and tested

--- cuda/softmax.cu
+++ cuda/softmax.cu
@@ -41,19 +41,51 @@ __global__ void crossentropy_forward_kernel1(const float* probs, const int* targ
     }
 }
 
+__global__ void crossentropy_softmax_backward_kernel1(const float* probs, const int* targets,
+                                                      float* dlogits, const float* dlosses,
+                                                      int B, int NC){
+    
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int b = idx/NC;
+    int nc = idx%NC;
+
+    if(idx < B*NC){
+        float dloss = dlosses[b];
+        float cls_idx = targets[b];
+
+        // dL/d(logits) = dL/d(prob) * d(probs)/d(logits)
+        // dL/d(probs) = -1/prob, d(probs)/d(logits) = softmax(target_logit)*(1-softmax(target_logit)) if logit == target
+        // otherwise, softmax(target_logit)*(0 - softmax(curr_logit))
+        // where prob = softmax output for target logit, and dL/d(prob) = 0 for all other logits.
+        // so, dL/d(logits) = -1/prob * (prob) * (indicator - curr_prob) =  curr_prob - indicator.
+
+        float curr_cls_prob = probs[b*NC + nc];
+        float indicator = nc == cls_idx ? 1.f : 0.f;
+        atomicAdd(&dlogits[b*NC + nc], (curr_cls_prob - indicator)*dloss);
+    }
+}
+
 // -----------------------------------------------------------------------------------------
 // kernel launcher
 
 void softmax_forward1(float* logits, float* probs,
                       int B, int NC, const int block_size){
     
-    const int grid_size = ceil_div(N, block_size);
+    const int grid_size = ceil_div(B, block_size);
     softmax_forward_kernel1<<<grid_size, block_size>>>(logits, probs, B, NC);
 }
 
 void crossentropy_forward1(const float* probs, const int* targets, float* losses,
                            int B, int NC, const int block_size){
     
-    const int grid_size = ceil_div(N, block_size);
+    const int grid_size = ceil_div(B, block_size);
     crossentropy_forward_kernel1<<<grid_size, block_size>>>(probs, targets, losses, B, NC);
+}
+
+void crossentropy_softmax_backward1(const float* probs, const int* targets,
+                                    float* dlogits, const float* dlosses,
+                                    int B, int NC, const int block_size){
+    
+    const int grid_size = ceil_div(B*NC, block_size);
+    crossentropy_softmax_backward_kernel1<<<grid_size, block_size>>>(probs, targets, dlogits, dlosses, B, NC);
 }
--- cuda/softmax.cuh
+++ cuda/softmax.cuh
@@ -15,6 +15,7 @@
 __global__ void softmax_forward_kernel1(float* logits, float* probs,
                                         int B, int NC);
 
+
 // crossentropy, forward kernel function 1
 //
 // @param probs         linearized input probability tensors (B, 1, NC)
@@ -25,6 +26,20 @@ __global__ void softmax_forward_kernel1(float* logits, float* probs,
 //
 __global__ void crossentropy_forward_kernel1(const float* probs, const int* targets, float* losses,
                                              int B, int NC);
+
+
+// crossentropy and softmax backward kernel function 1
+//
+// @param probs         linearized input probability tensors (B, 1, NC)
+// @param targets       linearized ground truth label tensors (B, 1, 1)
+// @param dlogits       linearized logit tensors derivatives (B, 1, NC)
+// @param dlosses       linearized losses tensors derivatives (B, 1, 1)
+// @param B             number of batches
+// @param NC            number of classes
+//
+__global__ void crossentropy_softmax_backward_kernel1(const float* probs, const int* targets,
+                                                      float* dlogits, const float* dlosses,
+                                                      int B, int NC);
 
 // -----------------------------------------------------------------------------------------
 // kernel launcher
@@ -40,6 +55,7 @@ __global__ void crossentropy_forward_kernel1(const float* probs, const int* targ
 void softmax_forward1(float* logits, float* probs,
                       int B, int NC, const int block_size);
 
+
 // crossentropy forward kernel launcher 1
 //
 // @param probs         linearized input probability tensors (B, 1, NC)
@@ -51,3 +67,18 @@ void softmax_forward1(float* logits, float* probs,
 //
 void crossentropy_forward1(const float* probs, const int* targets, float* losses,
                            int B, int NC, const int block_size);
+
+
+// crossentropy and softmax backward kernel launcher 1
+//
+// @param probs         linearized input probability tensors (B, 1, NC)
+// @param targets       linearized ground truth label tensors (B, 1, 1)
+// @param dlogits       linearized logit tensors derivatives (B, 1, NC)
+// @param dlosses       linearized losses tensors derivatives (B, 1, 1)
+// @param B             number of batches
+// @param NC            number of classes
+// @param block_size    CUDA block size
+//
+void crossentropy_softmax_backward1(const float* probs, const int* targets,
+                                    float* dlogits, const float* dlosses,
+                                    int B, int NC, const int block_size);
