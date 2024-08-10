111aab16a69bbf1e3d924ac6d692f6ddf2791c9e
Dongjin Kim <djkim9031.research@gmail.com>
2024-08-10 09:33:52 -0400

    softmax-crossentropy loss calculation test added

--- test/softmax.cpp
+++ test/softmax.cpp
@@ -33,6 +33,61 @@ TEST(SoftmaxTest, forward_call){
     }
 }
 
+// softmax-crossentropy forward call.
+TEST(SoftmaxCrossentropyTest, forward_call){
+    // (2, 1, 10)
+    float logits[20] = {0.01, -0.1, 0.2, 0.3, -0.4, 0.31, 0.12, -0.15, 0.9, 0.2,
+                        -0.8, -0.4, 0.12, 0.76, 0.14, -0.27, 0.3, 0.1, 0.5, 0.2};
+    
+    float probs[20] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                       0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+
+    // (2, 1, 1)
+    int targets[2] = {8, 8};
+    float losses[2] = {0.0, 0.0};
+
+    // losses_truth is pytorch tensor output.
+    /* Python code
+
+    import torch
+    from torch import nn
+    import torch.nn.functional as F
+
+    torch.set_printoptions(precision=6)
+
+    # Define logits and targets
+    logits = [0.01, -0.1, 0.2, 0.3, -0.4, 0.31, 0.12, -0.15, 0.9, 0.2,
+            -0.8, -0.4, 0.12, 0.76, 0.14, -0.27, 0.3, 0.1, 0.5, 0.2]
+    logits = torch.tensor(logits, dtype=torch.float32).view(2, 10)
+    logits = logits.requires_grad_()
+
+    targets = [8, 8]
+    targets = torch.tensor(targets, dtype=torch.long)
+
+    # Custom Cross-Entropy loss function
+    def custom_cross_entropy_loss(logits, targets):
+        probs = F.softmax(logits, dim=-1)
+        log_probs = torch.log(probs)
+
+        target_log_probs = -log_probs[range(logits.size(0)), targets]
+
+        return target_log_probs
+
+    loss = custom_cross_entropy_loss(logits, targets)
+
+    print("loss: ", loss)
+    */
+
+    float losses_truth[2] = {1.600543, 1.953092};
+    float tolerance = 5e-6;
+
+    softmax_forward(logits, probs, 2, 10);
+    crossentropy_forward(probs, targets, losses, 2, 10);
+    for(int i=0; i<2; ++i){
+        EXPECT_NEAR(losses[i], losses_truth[i], tolerance);
+    }
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
