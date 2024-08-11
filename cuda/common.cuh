0c46d0722363ac5ea19a3b54ff878ffb37df0676
Dongjin Kim <djkim9031.research@gmail.com>
2024-08-11 10:23:09 -0400

    utils updates

--- cuda/common.cuh
+++ cuda/common.cuh
@@ -15,7 +15,10 @@ __host__ __device__ T ceil_div(T dividend, T divisor){
     return (dividend + divisor - 1)/divisor;
 }
 
+// ----------------------------------------------------------------------------
 // Reduced/Mixed precision utilities
+
+// precision datatype definition
 #if defined(ENABLE_BF16)
 typedef __nv_bfloat16 floatX;
 typedef __nv_bfloat16 floatN;
@@ -29,3 +32,16 @@ typedef float floatX;
 typedef float floatN;
 
 #endif
+
+// ----------------------------------------------------------------------------
+// checking utils
+
+// CUDA error checking
+inline void cuda_check(cudaError_t error, const char *file, int line) {
+    if (error != cudaSuccess) {
+        printf("[CUDA ERROR] at file %s:%d:\n%s\n", file, line,
+               cudaGetErrorString(error));
+        exit(EXIT_FAILURE);
+    }
+};
+#define cudaCheck(err) (cuda_check(err, __FILE__, __LINE__))
